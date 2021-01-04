b89f14abe9ddc387eeba99d7a2804bf85a05432d
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-01-04 21:57:18 +0100

    script

---
+++ code.js
@@ -0,0 +1,16 @@
+const btn = document.querySelector(`button`);
+let number = 1;
+
+const addEl = () => {
+  const li = document.createElement("li");
+  li.textContent = number;
+
+  if (number % 3 === 0) {
+    li.classList.add("big");
+  }
+
+  document.querySelector(`ul`).appendChild(li);
+  number += 2;
+};
+
+btn.addEventListener(`click`, addEl);
